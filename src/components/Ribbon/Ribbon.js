c114f29e25e22761f660c95ae9a80f0563edbf09
kacper <kacpersloka@o2.pl>
2023-02-01 17:52:06 +0100

    stuff 2

--- src/components/Ribbon/Ribbon.js
+++ src/components/Ribbon/Ribbon.js
@@ -1,10 +1,13 @@
 import React from "react";
 import css from './Ribbon.module.css';
+import Clipboard from '../Clipboard/Clipboard';
 
 function Ribbon() {
   return (
     <div className={css['container']}>
-
+      <Clipboard/>
+      <Clipboard/>
+      <Clipboard/>
     </div>
   );
 }
