6f385b56431bf465744b9649f34a88f70438c105
kacper <kacpersloka@o2.pl>
2023-02-12 19:10:38 +0100

    very well

--- src/components/ShapesGrid/ShapesGrid.js
+++ src/components/ShapesGrid/ShapesGrid.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import PropTypes from 'prop-types';
 import css from './ShapesGrid.module.css';
+
+import useOutsideClick from "../../hooks/useOutsideClick";
+import { toggleBoolState } from "../../utils/utils";
 
 import callout16 from './assets/callout-16.png';
 import cloudCallout16 from './assets/cloud-callout-16.png';
@@ -25,110 +28,153 @@ import roundedRectangle16 from './assets/rounded-rectangle-16.png';
 import sixPointStar16 from './assets/six-point-star-16.png';
 import triangle16 from './assets/triangle-16.png';
 import upArrow16 from './assets/up-arrow-16.png';
+import { ReactComponent as TriangleDown } from '../../assets/global/triangle-down.svg';
+import { ReactComponent as TriangleLine } from '../../assets/global/triangle-line.svg';
 
 function ShapesGrid() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+  const dropdownRef = useRef();
+  useOutsideClick(dropdownRef, () => isDropdownOpen && setIsDropdownOpen(false));
+  
   return (
     <div className={css['container']}>
       <div className={css['grid']}>
-        <button className="button">
-          <img src={line16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={curve16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={oval16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={rectangle16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={roundedRectangle16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={polygon16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={triangle16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={rightTriangle16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={diamond16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={pentagon16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={hexagon16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={rightArrow16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={leftArrow16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={upArrow16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={downArrow16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={fourPointStar16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={fivePointStar16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={sixPointStar16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={callout16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={ovalCallout16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={cloudCallout16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={heart16} alt=""/>
-        </button>
-
-        <button className="button">
-          <img src={lightning16} alt=""/>
-        </button>
+        <Shapes/>
       </div>
 
       <div>
-        <button className={css['button']}>1</button>
-        <button className={css['button']}>2</button>
-        <button className={css['button']}>3</button>
+        <button className={`${css['button']} ${css['button--disabled']}`}>
+          <TriangleDown className="rotate-180"/>
+        </button>
+        <button className={css['button']}>
+          <TriangleDown/>
+        </button>
+        <button 
+          className={css['button']}
+          onPointerDown={() => toggleBoolState(isDropdownOpen, setIsDropdownOpen)}
+        >
+          <TriangleLine/>
+        </button>
       </div>
+
+      {
+        isDropdownOpen &&
+          <div className={`${css['dropdown']} popup`} ref={dropdownRef}>
+            <div className={css['expanded']}>
+              <div className={css['expanded__grid']}>
+                <Shapes/>
+              </div>
+
+              <div className={css['expanded__scrollbar']}>
+                <button className={`${css['expanded__scrollbar__button']} ${css['expanded__scrollbar__button--disabled']}`}>
+                  <TriangleDown className="rotate-180"/>
+                </button>
+                <button className={`${css['expanded__scrollbar__button']} ${css['expanded__scrollbar__button--disabled']}`}>
+                  <TriangleDown/>
+                </button>
+              </div>
+            </div>
+          </div>
+      }
     </div>
+  );
+}
+
+function Shapes() {
+  return (
+    <>
+      <button className="button">
+        <img src={line16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={curve16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={oval16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={rectangle16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={roundedRectangle16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={polygon16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={triangle16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={rightTriangle16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={diamond16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={pentagon16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={hexagon16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={rightArrow16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={leftArrow16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={upArrow16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={downArrow16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={fourPointStar16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={fivePointStar16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={sixPointStar16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={callout16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={ovalCallout16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={cloudCallout16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={heart16} alt=""/>
+      </button>
+
+      <button className="button">
+        <img src={lightning16} alt=""/>
+      </button>
+    </>
   );
 }
 
