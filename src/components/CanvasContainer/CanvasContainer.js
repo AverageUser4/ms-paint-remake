34d1c427ca4f7ff4fd644807bf6152504fefc42a
kacper <kacpersloka@o2.pl>
2023-03-12 10:29:57 +0100

    work on rulers, still not done

--- src/components/CanvasContainer/CanvasContainer.js
+++ src/components/CanvasContainer/CanvasContainer.js
@@ -5,10 +5,13 @@ import css from './CanvasContainer.module.css';
 import Canvas from '../Canvas/Canvas';
 import Rulers from '../Rulers/Rulers';
 import { useWindowsContext } from '../../misc/WindowsContext';
+import { useCanvasContext } from '../../misc/CanvasContext';
 
 function CanvasContainer({ toolbarData, ribbonData }) {
+  // called so it rerenders when canvas size changes
+  useCanvasContext();
   const { isStatusBarVisible, isRulersVisible } = useWindowsContext();
-  const canvasContainerRef = useRef();
+  const containerRef = useRef();
   
   const containerStyle = {
     height: `calc(
@@ -18,7 +21,6 @@ function CanvasContainer({ toolbarData, ribbonData }) {
       - var(--topbar-height)
       ${isStatusBarVisible ? '- var(--statusbar-height)' : ''}
       ${toolbarData.reposition ? '- var(--qa-toolbar-height)' : ''}
-      ${isRulersVisible ? '- var(--rulers-size)' : ''}
     )`
   };
 
@@ -28,15 +30,11 @@ function CanvasContainer({ toolbarData, ribbonData }) {
         ${css['container']}
         ${isRulersVisible && css['container--has-rulers']}
       `}
+      ref={containerRef}
       style={containerStyle}
     >
-      <Rulers/>
-      <div 
-        className={css['canvas-container']}
-        ref={canvasContainerRef}
-      >
-        <Canvas/>
-      </div>
+      {isRulersVisible && <Rulers/>}
+      <Canvas/>
     </div>
   );
 }
--- src/components/Rulers/Rulers.js
+++ src/components/Rulers/Rulers.js
@@ -4,34 +4,44 @@ import css from './Rulers.module.css';
 import { useCanvasContext } from '../../misc/CanvasContext';
 
 function Rulers() {
-  const { canvasZoom } = useCanvasContext();
+  const { canvasZoom, canvasSize } = useCanvasContext();
   
   let fullStep = 125;
-  
-  if(canvasZoom >= 0.5) {
-    fullStep = 100;
-  }
-  if(canvasZoom === 3) {
-    fullStep = 60;
-  }
-  if(canvasZoom === 4) {
-    fullStep = 80;
-  }
-  if(canvasZoom === 5) {
-    fullStep = 100;
-  }
-  if(canvasZoom >= 6) {
-    fullStep = canvasZoom * 10;
-  }
-
+  if(canvasZoom >= 0.5) { fullStep = 100; }
+  if(canvasZoom === 3) { fullStep = 60; }
+  if(canvasZoom === 4) { fullStep = 80; }
+  if(canvasZoom === 5) { fullStep = 100; }
+  if(canvasZoom >= 6) { fullStep = canvasZoom * 10; }
   let subStep = fullStep / 10;
 
+  let stepValue = 1000;
+  if(canvasZoom === 0.25) { stepValue = 500; }
+  if(canvasZoom === 0.5) { stepValue = 200; }
+  if(canvasZoom === 1) { stepValue = 100; }
+  if(canvasZoom === 2) { stepValue = 50; }
+  if(canvasZoom >= 3) { stepValue = 20; }
+  if(canvasZoom >= 6) { stepValue = 10; }
+
+  const rulerWidth = canvasSize.width;
+  
   const textArray = [];
+  let i = 0;
   // should be ruler width instead of window inner width
-  for(let x = 0; x < window.innerWidth; x += fullStep) {
+  for(let x = 0; x < rulerWidth; x += fullStep) {
     textArray.push(
-      <text x={x} y="10">100</text>
+      <text
+        className="text"
+        key={x}
+        x={x + 5}
+        y="11"
+        style={{ fontSize: 12 }}
+        fill="rgb(51, 75, 106)"
+      >
+        {stepValue * i}
+      </text>
     );
+
+    i++;
   }
 
   
@@ -40,6 +50,9 @@ function Rulers() {
       <svg 
         className={`${css['ruler']} ${css['ruler--top']}`}
         xmlns="http://www.w3.org/2000/svg"
+        style={{
+          width: `calc(${rulerWidth}px - var(--canvas-container-padding) - var(--rulers-size))`
+        }}
       >
         <defs>
           <pattern id="helperPatternTop" width={subStep} height={17} patternUnits="userSpaceOnUse">
